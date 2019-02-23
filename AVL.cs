75390b3979d0bd68894e45255e803381440c6e7e
RimaShahbazyan <s.rima2000rs@gmail.com>
2019-02-23 21:14:08 +0400

    AVL

---
+++ AVL.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace DS_CSHARP
+{
+    public class AVL
+    {
+        public AVL()
+        {
+        }
+        private  Node root;
+        private  Node addAsBST(int value)
+        {
+            Node newNode = new Node(value);
+            if (root == null)
+            {
+                root = newNode;
+                return newNode;
+            }
+
+            Node current=root;
+            while (true)
+            {
+                if (current.Value >= newNode.Value)
+                {
+                    if (current.LeftChild == null)
+                    {
+                        current.LeftChild = newNode;
+                        return newNode;
+                    }
+                    current = current.LeftChild;
+                }
+                else
+                {
+                    if (current.RightChild == null)
+                    {
+                        current.RightChild = newNode;
+                        return newNode;
+                    }
+                    current = current.RightChild;
+                }
+            }
+        }
+
+        private void rotateRight(Node x)
+        {
+             if(x.IsLeftChild && x.Parent != null)
+                x.Parent.LeftChild= x.LeftChild;
+
+            else if (x.Parent != null)
+                x.Parent.RightChild = x.LeftChild;
+
+            x.LeftChild.Parent = x.Parent;
+            x.LeftChild = x.LeftChild.RightChild;
+            x.LeftChild.RightChild = x;
+
+        }
+        private void rotateLeft(Node x)
+        {
+            if (x.IsLeftChild && x.Parent!=null)
+                x.Parent.LeftChild = x.RightChild;
+
+            else if(x.Parent != null)
+                x.Parent.RightChild = x.RightChild;
+
+            x.RightChild.Parent = x.Parent;
+            x.LeftChild = x.RightChild.RightChild;
+            x.RightChild.LeftChild = x;
+
+        }
+
+        public void AddNode(int value)
+        {
+            Node current = addAsBST(value);
+            while(current!= null) 
+            { 
+                if(current.RightChild.Height - current.LeftChild.Height >1 )
+                { 
+                    rotateLeft(current);
+                }
+                if (current.RightChild.Height - current.LeftChild.Height <-1 )
+                {
+                    rotateRight(current);
+                }
+                current = current.Parent;
+            }
+        }
+
+        private class Node
+        {
+            public Node(int num)
+            {
+                Value = num;
+            }
+
+
+            public int Value { get; private set; } 
+
+            public Node Parent { get; set; }
+
+            public Node LeftChild { get; set; }
+            public Node RightChild { get; set; }
+
+            public int Height { get { return getHeight(); } }
+            private int getHeight ()
+            {
+                if(this.LeftChild== null && this.RightChild == null)
+                {
+                    return -1;
+                }
+                if (this.LeftChild == null)
+                {
+                    return this.RightChild.getHeight()+1;
+                }
+                if (this.RightChild == null)
+                {
+                    return this.LeftChild.getHeight() + 1;
+                }
+
+                return Math.Max(this.LeftChild.getHeight() , this.RightChild.getHeight())+1;
+            }
+            public bool IsLeftChild { get { return this.Parent.LeftChild.Value == this.Value; } }
+
+        }
+    }
+}

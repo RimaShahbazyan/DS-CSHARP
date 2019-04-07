68eadeac2c2e10e19ed6cf434631b2b2d873db0e
RimaShahbazyan <s.rima2000rs@gmail.com>
2019-04-07 21:03:53 +0400

    մե հրաշք HashTable

---
+++ HashTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DS_CSHARP
+{
+    public abstract class HashTable<K, V>:IMyCollection<K,V> where K: IComparable<K>
+    {
+        private List<E>[] arr;
+        public HashTable(int arrSize)
+        {
+            Size = 0;
+            arr = new List<E>[arrSize];
+        }
+        public int Size { get; private set; }
+        public bool IsEmpty { get { return Size == 0; } }
+        protected abstract long GetHash(K key);
+        protected abstract int Compress(long Hash);
+        public void Insert(K key, V value) 
+        {
+            E entry = new E(key, value);
+            arr[Compress(GetHash(key))].Add( entry);
+         
+        }
+        public V Search (K key)
+        {
+            return searchE(key).Value;
+        }
+
+        public bool Remove(K key) {
+            return arr[Compress(GetHash(key))].Remove(searchE(key));
+        }
+
+        private E searchE(K key)
+        {
+            return arr[Compress(GetHash(key))].Find(e => e.Key.CompareTo(key) == 0);
+        }
+        private class E
+        {
+            public E( K key, V value)
+            {
+                this.Key = key;
+                this.Value = value;
+            }
+            public K Key { get; private set; }
+            public V Value { get; private set; }
+        }
+
+    }
+
+}
---
+++ IMyCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DS_CSHARP
+{
+    public interface IMyCollection<K,V>
+    {
+        void Insert(K key, V value);
+        bool Remove( K key);
+        V Search(K key);
+        int Size{ get;  }
+        bool IsEmpty{ get; }
+    }
+
+}
